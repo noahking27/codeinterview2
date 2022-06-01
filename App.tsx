64bbfa075ed97d73deaadd4200e51114ee56c9e2
Noah King <noahking21@gmail.com>
2022-06-01 12:34:12 -0400

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import "./styles.css";
+
+/**
+ * Instructions:
+ *
+ * Create a form with the following:
+ *   "Name" input element
+ *   "Role" select element
+ *      The role values are "Admin", "User", and "Manager"
+ *   "Submit" button
+ *
+ * After submitting, the form data should be displayed on the page instead of the form
+ *
+ * See form.png for the required styling for the form
+ *
+ * See results.png for the minimum output. Add extra styling if time permits.
+ */
+
+export const App: React.FunctionComponent = () => {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+};
